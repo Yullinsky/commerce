9ba4b7477be7ff5df6b69823237fd78b561520d1
Yullinsky <arturoespejo72@gmail.com>
2025-10-29 19:28:52 -0600

    fix: remove unnecessary line in models.py

--- auctions/models.py
+++ auctions/models.py
@@ -13,7 +13,7 @@ class Listing(models.Model) :
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     category = models.CharField (max_length=100, blank=True, null=True)
     is_active = models.BooleanField (default=True)
-89
+
 class Bid(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
