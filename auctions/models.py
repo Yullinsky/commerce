bc04229363ee67d5270c5fc9852f39a760f2bd35
Yullinsky <arturoespejo72@gmail.com>
2025-10-29 19:27:32 -0600

    hotfix models.py

---
+++ auctions/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model) :
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    category = models.CharField (max_length=100, blank=True, null=True)
+    is_active = models.BooleanField (default=True)
+89
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    timestamp = models.DateField(auto_now_add=True)
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField (max_length=1000)
+    listings = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    timestamp = models.DateField(auto_now_add=True)     
